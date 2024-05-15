462157d8dd9909eb9149fe1d1e07fc6aba094d66
Lukas Rusak <lorusak@gmail.com>
2024-05-15 10:06:40 -0700

    test_eagle200.py: remove unneeded pprint
    Signed-off-by: Lukas Rusak <lorusak@gmail.com>

--- tests/test_eagle200.py
+++ tests/test_eagle200.py
@@ -1,7 +1,5 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
-from pprint import pprint
-
 import pytest
 from flask import url_for
 
@@ -25,24 +23,20 @@ class TestLiveServer:
         conn = libeagle.Connection(url.hostname, "0077dd", "6e61a3a94882eef9", port=url.port, debug=True)
 
         devices = conn.device_list()
-        pprint(devices)
 
         details = conn.device_details(devices[0]["HardwareAddress"])
-        pprint(details)
 
         query = conn.device_query(
             devices[0]["HardwareAddress"],
             details[0]["Name"],
             details[0]["Variables"][0],
         )
-        pprint(query)
 
         assert (
             query[0]["Variables"]["zigbee:InstantaneousDemand"] == "21.499 kW"
         )
 
         query = conn.device_query(devices[0]["HardwareAddress"])
-        pprint(query)
 
         assert (
             query[0]["Variables"]["zigbee:Message"] == "Hello, World!"
